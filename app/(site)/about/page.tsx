52a71733af5cfa52ac95c20625f79c2362e8b6fb
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-10-05 13:12:06 +0100

    d

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const about = () => {
   return (
   <div className="prose lg:prose-xl">
-    <p>I've been at it for a while.</p>
+    <p>I&apos;ve been at it for a while.</p>
   </div>
   );
 };
