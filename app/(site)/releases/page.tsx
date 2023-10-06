5b47cecbf1f49b2e9de36d8c0c405b2399839501
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-10-06 15:26:08 +0100

    aa

--- app/(site)/releases/page.tsx
+++ app/(site)/releases/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { Spotify } from "react-spotify-embed"
 
 const releases = () => {
   return (
     <div>
       <h1 className='mb-8'>Release archive, to be populated soon, here is a Spotify playlist of my stuff for now.</h1>
-      <iframe className="rounded" src="https://open.spotify.com/embed/artist/7xqnFwk6ZueWJ1IA8gsEWM?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      {/* <iframe className="rounded" src="https://open.spotify.com/embed/artist/7xqnFwk6ZueWJ1IA8gsEWM?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
+      <Spotify link="https://open.spotify.com/artist/7xqnFwk6ZueWJ1IA8gsEWM?si=mM0-nGxST065udFfBTZbMA" width='100%'/>
     </div>
   )
 }
