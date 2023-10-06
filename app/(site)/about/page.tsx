94f1cdfae2d22c59d4ae723d578c05d11ea1f695
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-10-06 16:13:19 +0100

    d

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -8,7 +8,7 @@ const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 const about = () => {
   return (
     <div className="prose lg:prose-xl">
-      <p className="text-sm mb-6">I&#39;ve been a professional producer and DJ for over a decade and have more recently started working within composition roles. If you&#39;d like chat about a project, don&#39;t hesitate to <Link href="/contact">get in touch</Link>.</p>
+      <p className="text-sm mb-6">I&#39;ve been a professional producer and DJ for over a decade and have more recently started working as a composer. If you&#39;d like chat about a project, don&#39;t hesitate to <Link href="/contact">get in touch</Link>.</p>
       {/* <ReactPlayer
         className=""
         url="https://soundcloud.com/joy-orbison/tentative-bidding-demo"
